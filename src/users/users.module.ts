c57cbefc6c47dc30dd5694e7d1c72ec6324be242
oktapascal <oktaiscool@gmail.com>
2023-08-25 07:23:36 +0700

    Feat: add import ConfigModule in users.module.ts file

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,23 @@ import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
+import { AccessTokenStrategy, RefreshTokenStrategy } from './strategies';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from '../common/guards';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
-  imports: [JwtModule.register({})],
-  providers: [AuthService, UsersService],
+  imports: [ConfigModule, JwtModule.register({})],
+  providers: [
+    AuthService,
+    UsersService,
+    AccessTokenStrategy,
+    RefreshTokenStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
+    },
+  ],
 })
 export class UsersModule {}
